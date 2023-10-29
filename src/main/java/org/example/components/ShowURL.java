a3878e0dafe16244549f3b75adfe025032cc0a06
DeymonDik <deymon91@gmail.com>
2023-10-29 20:23:15 +0300

    Master

--- src/main/java/org/example/components/ShowURL.java
+++ src/main/java/org/example/components/ShowURL.java
@@ -1,29 +1,54 @@
 package org.example.components;
 
-import lombok.Setter;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import java.awt.*;
+import java.net.InetAddress;
 import java.net.URI;
 import java.util.Arrays;
 
 @Component
 @Slf4j
-@Setter
+@AllArgsConstructor
 public class ShowURL implements ApplicationListener<ApplicationReadyEvent> {
 
-    @Value("${url}")
-    private String url;
+    private SwaggerUiConfigParameters swaggerUiConfigParameters;
+    private ServerProperties serverProperties;
+    private AnnotationConfigServletWebServerApplicationContext context;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         try {
+
+            String scheme = serverProperties.getSsl() != null && serverProperties.getSsl().isEnabled()
+                    ? "https" : "http";
+
+            String host = (serverProperties.getAddress() == null
+                    ? InetAddress.getLocalHost() : serverProperties.getAddress())
+                    .getHostAddress();
+
+            int port = context.getWebServer().getPort();
+
+            String path = swaggerUiConfigParameters.getPath();
+
+            URI uri = new URI(scheme,
+                    null,
+                    host,
+                    port,
+                    path,
+                    null,
+                    null);
+
             System.setProperty("java.awt.headless", "false");
-            Desktop.getDesktop().browse(new URI(url));
+            Desktop.getDesktop().browse(uri);
+
         } catch (Exception e) {
             log.error(Arrays.toString(e.getStackTrace()));
         } finally {
